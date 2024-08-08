0f5b2f3c69b7ce38883f4f4c5aaac9ba4dc27eec
Ian Too <ianktoo@gmail.com>
2024-08-08 20:19:24 +0300

    New App

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ config('app.name', 'Application')}}</title>
+</head>
+<body>
+    <p>Complaints Information and Tracking System</p>composer require laravel/breeze --dev
+</body>
+</html>
